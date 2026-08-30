29a72844ecb9b494e14ce9fd8cc3e8e2165faf9f
chaudhariprincekumar66-clod <chaudhariprincekumar66@gmail.com>
2026-08-30 17:10:01 +0530

    Created an Assignment01.py

---
+++ Python_Assignment/Assignment01.py
@@ -0,0 +1,17 @@
+Question:1
+name = "Raju"
+print("Name:", name)
+Answer:1
+Name: Raju
+Question:2
+student_name = "Aman"
+print("Student Name:", student_name)
+Answer:2
+Student_Name: Aman
+Question:3
+college_name = "ABC College"
+student_name = "Meera"
+
+print("College:", college_name)
+print("Student:", student_name)
+

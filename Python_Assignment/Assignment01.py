a531a9d390ce5b10020fd7b89d351faccd611d26
chaudhariprincekumar66-clod <chaudhariprincekumar66@gmail.com>
2026-08-31 13:01:33 +0530

    modify Assignment01 file

--- Python_Assignment/Assignment01.py
+++ Python_Assignment/Assignment01.py
@@ -14,4 +14,123 @@ student_name = "Meera"
 
 print("College:", college_name)
 print("Student:", student_name)
+Answer:3
+College: ABC_College
+Student: Meera
+Question:4
+first_name = "Raj"
+last_name = "Patel"
+
+print("Full Name:", first_name, last_name)
+Answer:4
+Full_Name: "Raj_Patel"
+Question:5
+Name="Rahul_Raj"
+Age=18
+Course="B.Tech"
+City="Patna"
+"Print_these_in_a_sentence?"
+Answer:5
+print("My name is",Name,".I am 18 year old",Age,".I am studying",Course,"and I live in",City,".")
+Question:6
+Name="Priya sharma"
+Age="19"
+Gender="Female"
+City="Jaipur"
+"Print_these_in_a_sentence?"
+Answer:6
+print("my name is ",Name,".I am",Age,"years old . I am ",Gender," and I live in",City,".")
+Question:7
+College="ABC Intitute of technology"
+Branch="Computer Science"
+Semester="1"
+city="Ahmedabad"
+Persentage="87.5"
+Programming_Language="Python"
+"Print_these_in_a_sentence?"
+Answer:7
+print("My name is "+Name+". I am "+Age+" old and I am studying "+Branch+" in semester"+Semester+" at "+College,city+".I have "+Persentage+"persentage. I am also know "+Programming_Language)
+Question:8
+name = "Riya"
+age = 20
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+Answer:8
+Name_Type= "str"
+Age_Type="int"
+Marks_Type="float"
+Question:9
+name = "Ravi"
+age = 18
+marks = 90.5
+passed = True
+
+print("Name:", name)
+print("Age:", Age)
+print("Marks:", marks)
+print("Passed:", passed)
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(passed))
+Answer:9
+Name_type="str"
+Age_Type="int"
+Marks_Type="float"
+passed_type="bool" 
+Question:10
+a = 10.5
+b = 2.5
+
+print(a - b)
+Answer:13.0
+Question:11
+a = 10.5
+b = 2.0
+
+print(a / b)
+Answer:11
+5.25
+Question:12
+price = 100
+quantity = 3
+
+print(price * quantity)
+Answer:12
+300
+Question:13
+#A shop has 47 products. Each box can contain 6 products.
+
+#Write a Python program to find:
+
+#Number of complete boxes
+#Number of remaining products
+Answer:13
+products=47
+box_contain=6
+no_of_boxes=(products/box_contain)
+remaining_products=(products%box_contain)
+print(no_of_boxes)
+print(remaining_products)
+Question:14
+#A number is 125. Find:
+
+#Its square using **
+#Its cube using **
+#Its remainder when divided by 10 using %
+Answer:14
+square=(125**2)
+cube=(125**3)
+remainder=(125%10)
+print(square)
+print(cube)
+print(remainder)
 
